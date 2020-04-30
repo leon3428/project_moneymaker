29b796ba1c38b583e239a01f08e1c626f6ae2a7a
Leon Uroic <leon.stjepan@gmail.com>
2020-04-30 12:19:12 +0200

    data1

---
+++ data_extractor.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+class Club:
+    def __init__(self, name, id, hashtag):
+        self.name = name
+        self.id = id
+        self.hashtag = hashtag
+
+df = pd.read_excel ('data.xlsx')
+df = df[(df.ID == 'ENG PL') & ((df.SEASON == '2018/2019') | (df.SEASON == '2017/2018') | (df.SEASON == '2016/2017') | (df.SEASON == '2015/2016') | (df.SEASON == '2014/2015') | (df.SEASON == '2013/2014'))]
+print (df.head())
+print(df.shape, df.shape[0]/380)
+
+df = df.drop('ID', 1)
+df.to_csv('dataset.csv', index = False)
+
+"""
+names = set([])
+
+for index, row in df.iterrows():
+    names.add(row.HOME)
+    names.add(row.AWAY)
+
+print(names)
+
+f = open("clubs.txt", "w")
+
+for i, name in enumerate(names):
+    print(name, 'hashtag: ', end = '')
+    hash = input()
+    f.write(name + ' ' + str(i) + ' ' + hash + '\n')
+
+f.close()
+"""
